a546bad6739986ccdfd139cb60f375cb9889c881
Matthew Keen <matthew@worlduk.com>
2018-05-14 23:59:52 +0100

    Changed signup wordage

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -37,7 +37,7 @@ export default class Login extends React.Component {
             <button className="button">Login</button>
           </form>
 
-          <Link to="/signup">Have an account?</Link>
+          <Link to="/signup">Register for an account</Link>
         </div>
       </div>
     );
